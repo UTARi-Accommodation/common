05c205f44ebf484a50bb261637128a23b3f1c332
GervinFung <gervinfungdaxuen@gmail.com>
2022-07-11 02:29:30 +0800

    feat(build & test): removed build folder and transpile on install instead & improved test setup

--- test/index.ts
+++ test/index.ts
@@ -4,8 +4,18 @@ import testNullableParser from './parser/nullable';
 import testSearchParser from './parser/search';
 import testContactValidation from './contact/allValueValid';
 
-testArrayParser();
-testCoordinateParser();
-testNullableParser();
-testSearchParser();
-testContactValidation();
+const tests: ReadonlyArray<readonly [() => void, 'only'?]> = [
+    [testArrayParser],
+    [testCoordinateParser],
+    [testNullableParser],
+    [testSearchParser],
+    [testContactValidation],
+];
+
+const selectedTests = tests.filter(([_, only]) => only);
+
+if (process.env.IS_CI && selectedTests.length) {
+    throw new Error('cannot have "only" in ci cd');
+}
+
+(!selectedTests.length ? tests : selectedTests).forEach(([test]) => test());
